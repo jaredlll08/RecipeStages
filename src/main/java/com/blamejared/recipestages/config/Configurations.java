1ce296122b4d80ab1212643e4a0d7f305eab179e
Jared <jaredlll08@gmail.com>
2020-07-18 04:19:47 +0200

    fix compile issues

--- src/main/java/com/blamejared/recipestages/config/Configurations.java
+++ src/main/java/com/blamejared/recipestages/config/Configurations.java
@@ -1,13 +1,12 @@
 package com.blamejared.recipestages.config;
 
 import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.ConfigManager;
 
-@Configurations(modid = "recipestages")
+@Config(modid = "recipestages")
 public class Configurations {
 
-    @Configurations.Comment("Toggle whether to display which stage is needed for a recipe to work in the JEI entries.")
-    @Configurations.Name("Show stage name")
+    @Config.Comment("Toggle whether to display which stage is needed for a recipe to work in the JEI entries.")
+    @Config.Name("Show stage name")
 
     public static boolean showStageName = true;
 }
