d6ab8741e44be1d001952d1e6e341721de7453c1
Max <maxh2709@gmail.com>
2022-09-02 21:32:12 +0200

    Remove references to CraftTweaker's GenericUtil

---
+++ src/main/java/com/blamejared/recipestages/RecipeStagesUtil.java
@@ -0,0 +1,11 @@
+package com.blamejared.recipestages;
+
+public class RecipeStagesUtil {
+    
+    @SuppressWarnings("unchecked")
+    public static <T> T cast(Object o) {
+        
+        return (T) o;
+    }
+    
+}
